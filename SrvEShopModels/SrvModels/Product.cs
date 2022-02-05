8aca88d9b1c0e7c09612ef32110353d5fc2995b7
Manik <Captaiblood@users.noreply.github.com>
2022-02-05 19:28:35 +0000

    Checkbox and input type databinding

--- SrvEShopModels/SrvModels/Product.cs
+++ SrvEShopModels/SrvModels/Product.cs
@@ -10,6 +10,7 @@ namespace SrvEShopModels.SrvModels
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public int Price { get; set; }
         public bool IsActive { get; set; }
     }
 }
